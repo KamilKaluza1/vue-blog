0d59c7e8df3ceeb4af5eed628a164d67d592c6e1
Kamil Kałuża <kamil321.94@gmail.com>
2023-02-16 17:24:09 +0100

    snap

---
+++ src/components/PostList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+import SinglePost from './SinglePost.vue'
+export default {
+    components: { SinglePost }, 
+    props: ['posts'],
+    setup(props){
+        onMounted(()=>console.log('component mounted'))
+    }
+}
+</script>
