7cea2149f24f07d2d7aac9db057afc718638232a
Kamil Kałuża <kamil321.94@gmail.com>
2023-02-17 16:41:41 +0100

    snap

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="create">
-        <form>
+        <form @submit.prevent="handleSubmit">
             <label>Title</label>
             <input v-model="title" type="text" required>
             <label>Content</label>
@@ -39,8 +39,27 @@ export default{
             }
             tag.value = ''
         }
+
+        const handleSubmit = async () => {
+            try{
+                let post = {
+                    title: title.value,
+                    body: body.value,
+                    tags: tags.value
+                }
+                await fetch('http://localhost:3000/posts',{
+                    method: 'POST',
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify(post)
+                })
+            }
+            catch{
+
+            }
+
+        }
         
-        return{title, body, tag, tags, handleKeydown}
+        return{title, body, tag, tags, handleKeydown, handleSubmit}
     }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,3 +24,11 @@ export default {
   },
 };
 </script>
+
+<style>
+.home{
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 10px;
+}
+</style>
